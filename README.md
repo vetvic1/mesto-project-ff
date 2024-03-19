569dd7dda2111e7bbdc5a02233472081133dae57
s.volkova <s.volkova@owen.ru>
2024-03-20 02:01:56 +0300

    первый коммит

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/vetvic1/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+// Функция создания карточки
+function createCard(data, deleteCard) {
+  // Клонируем шаблон
+  const cardTemplate = document.querySelector("#card-template").content;
+  const newCard = cardTemplate.querySelector(".card").cloneNode(true);
 
-// @todo: Функция создания карточки
+  const cardImage = newCard.querySelector(".card__image");
+  // Устанавливаем значения
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  newCard.querySelector(".card__title").textContent = data.name;
 
-// @todo: Функция удаления карточки
+  // Добавляем обработчик клика для кнопки удаления
+  const deleteButton = newCard.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => deleteCard(newCard));
 
-// @todo: Вывести карточки на страницу
+  return newCard;
+}
+// Функция удаления карточки
+function deleteCard(card) {
+  card.remove();
+}
+
+// Выводим карточку на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, deleteCard);
+  placesList.appendChild(cardElement);
+});
